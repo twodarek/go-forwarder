05cc87df573b88c45b4d49c261607c7dabb7b926
Thomas Wodarek <wodarekwebpage@gmail.com>
2018-10-07 04:01:57 -0400

    Make auth work

--- golink.py
+++ golink.py
@@ -13,18 +13,27 @@ class GoLink(webapp2.RequestHandler):
   def post(self):
     url = models.Redirect_Url(parent=redirect_key)
 
+    user = None
+    logging.info(users.get_current_user())
+
     if users.get_current_user():
       url.user = users.get_current_user()
+      user = users.get_current_user()
 
     url.to_url = self.request.get('to_url').encode('ascii','ignore')
     url.input_url = self.request.get('input_url').encode('ascii','ignore')
 
-    if users.get_current_user() and users.is_current_user_admin():
+    if user and user.email() == 'KC8ZPQ@gmail.com':
       url.put()
       self.redirect('/')
-    elif users.get_current_user():
+    elif user:
+      logging.debug(user)
+      # logging.info("User %s attempted to add to the database" % (users.get_current_user())
       logging.info("Unauth'd user attempted to add entry to database")
+      self.response.write("You are not authorised to perform this action")
+      self.response.set_status(403)
     else:
       logging.info("Anon user attempted to add to the database")
-    self.response.write("You are not authorised to perform this action")
-    self.response.set_status(403)
+      self.response.write("You are not authorised to perform this action")
+      self.response.set_status(403)
+
