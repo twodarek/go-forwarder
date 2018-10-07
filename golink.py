ab32d1f8bf6eb5be31dc3a70ba77e3eadd1e5fab
Thomas Wodarek <wodarekwebpage@gmail.com>
2018-10-07 03:32:03 -0400

    Checkout, attempting to make auth-only writes work

--- golink.py
+++ golink.py
@@ -19,5 +19,12 @@ class GoLink(webapp2.RequestHandler):
     url.to_url = self.request.get('to_url').encode('ascii','ignore')
     url.input_url = self.request.get('input_url').encode('ascii','ignore')
 
-    url.put()
-    self.redirect('/')
+    if users.get_current_user() and users.is_current_user_admin():
+      url.put()
+      self.redirect('/')
+    elif users.get_current_user():
+      logging.info("Unauth'd user attempted to add entry to database")
+    else:
+      logging.info("Anon user attempted to add to the database")
+    self.response.write("You are not authorised to perform this action")
+    self.response.set_status(403)
