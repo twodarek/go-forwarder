b21a79c46b7cbebb8f762f1ba865690a9ab4557d
Thomas Wodarek <wodarekwebpage@gmail.com>
2019-10-07 22:35:30 -0400

    Add documentation

--- admin.py
+++ admin.py
@@ -9,6 +9,8 @@ from google.appengine.ext import ndb
 from google.appengine.api import users
 
 class AdminPage(webapp2.RequestHandler):
+	"""Render the admin page.  Currently a stub, but will expect at some point when we want to add a real admin page"""
+
   def get(self):
   	self.response.out.write("Hi Admin!")
 
--- models.py
+++ models.py
@@ -3,8 +3,16 @@ from google.appengine.api import users
 
 
 class Redirect_Url(ndb.Model):
+
+  # Username
   user = ndb.UserProperty()
+
+  # Destination url
   to_url = ndb.TextProperty(required=True, indexed=True)
+
+  # Url as input by the user
   input_url = ndb.TextProperty(required=True, indexed=True)
+
+  # Computed field to compare against when making a request.  Forced to lower case
   input_url_lower = ndb.ComputedProperty(lambda self: self.input_url.lower())
 
